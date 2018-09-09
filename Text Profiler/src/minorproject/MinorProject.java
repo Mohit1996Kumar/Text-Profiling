ce2c1ca972c64e837d92e77a3f4e4416e60860a4
Mohit Kumar <43097997+Mohit1996Kumar@users.noreply.github.com>
2018-09-10 01:03:16 +0530

    Added a new project

---
+++ Text Profiler/src/minorproject/MinorProject.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minorproject;
+
+/**
+ *
+ * @author Mohit Kumar Singla
+ */
+public class MinorProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainForm obj=new MainForm();
+        obj.setVisible(true);
+        
+    }
+    
+}
